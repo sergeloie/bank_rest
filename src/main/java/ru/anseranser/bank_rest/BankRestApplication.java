aaeb4cf53b000e4a199ba168986e50baa0d2c584
admin <serge.loie@yandex.ru>
2026-06-26 16:34:07 +0500

    initial commit

---
+++ src/main/java/ru/anseranser/bank_rest/BankRestApplication.java
@@ -0,0 +1,13 @@
+package ru.anseranser.bank_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankRestApplication.class, args);
+    }
+
+}
