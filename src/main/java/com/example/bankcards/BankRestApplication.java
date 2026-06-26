bd005aacd30411473463576dab5be4e34a4ba01e
admin <serge.loie@yandex.ru>
2026-06-26 16:39:34 +0500

    remove unused package

--- src/main/java/com/example/bankcards/BankRestApplication.java
+++ src/main/java/com/example/bankcards/BankRestApplication.java
@@ -1,4 +1,4 @@
-package ru.anseranser.bank_rest;
+package com.example.bankcards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/bankcards/BankRestApplicationTests.java
+++ src/test/java/com/example/bankcards/BankRestApplicationTests.java
@@ -1,4 +1,4 @@
-package ru.anseranser.bank_rest;
+package com.example.bankcards;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
