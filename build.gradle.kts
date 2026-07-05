e4e35383a2f6e18a95ca63b18d2f7ee14ba3052f
Sergey Gusev <serge.loie@yandex.ru>
2026-07-05 16:51:18 +0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     testImplementation("org.springframework.security:spring-security-test")
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testRuntimeOnly("com.h2database:h2")
 
     testCompileOnly("org.projectlombok:lombok")
     testAnnotationProcessor("org.projectlombok:lombok")
